3bbede4dba3a6e52bb144e798ad110eac7829fcf
Jeronimo-Pelicoli <jeronimosama@gmail.com>
2021-07-21 15:20:17 -0300

    test heroku

--- src/main/java/com/diospring/diospringsantander/controllers/PersonController.java
+++ src/main/java/com/diospring/diospringsantander/controllers/PersonController.java
@@ -14,7 +14,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/people")
-@AllArgsConstructor(onConstructor = @__({@Autowired}))
+@AllArgsConstructor(onConstructor_ = {@Autowired})
 public class PersonController {
 
 //    @Autowired
